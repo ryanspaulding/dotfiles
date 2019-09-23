5716871f06d93fdd291336420ba91fc96a2a7c66
Ryan Spaulding <ryanspaulding@gmail.com>
2019-09-22 21:11:23 -0700

    fixing some syntax issues

--- bashrc.bash
+++ bashrc.bash
@@ -2,9 +2,9 @@
 # Aliases
 alias ll="ls -l"
 alias starttmux="tmux new -s `hostname`"
-# Path setup
+
 # rbenv Setup
-+eval "$(rbenv init -)"
-+
+eval "$(rbenv init -)"
+
  # Path setup
-+export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
