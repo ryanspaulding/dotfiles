d8254d0edcbcd5e55acc8e471b1c657d630934ae
Ryan Spaulding <ryanspaulding@gmail.com>
2019-10-26 05:57:10 -0700

    added pwgen magic

--- bashrc.bash
+++ bashrc.bash
@@ -3,8 +3,14 @@
 alias ll="ls -l"
 alias starttmux="tmux new -s `hostname`"
 
+ # Path setup
+export PATH="$HOME/.rbenv/bin:$PATH"
+
 # rbenv Setup
 eval "$(rbenv init -)"
 
- # Path setup
-export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="/home/ryanspaulding/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+alias pw="pwgen -ncy 12 1"
